04bf49a70d0bff438dfd79589b3c028f2a8f6ea9
Alessio Poggi <alessio.poggi01@universitadipavia.it>
2023-09-18 16:44:56 +0200

    added dynamic theme color

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -3,4 +3,7 @@ export const applyThemePreference = (theme: string) => {
     const isDark = theme === 'dark'
     root.classList.remove(isDark ? 'light' : 'dark')
     root.classList.add(theme)
+    
+    const metaTheme = window.document.querySelector('meta[name="theme-color"]')
+    if(metaTheme) metaTheme.setAttribute('content', isDark ? '#242424' : '#e3e3e1')
 }
