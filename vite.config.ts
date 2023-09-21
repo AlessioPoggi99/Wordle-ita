571a060a6f0d84166b17721d0f1967970e1d0f44
Alessio Poggi <alessio.poggi01@universitadipavia.it>
2023-09-21 16:43:24 +0200

    removed standard manifest and added to pwa plugin options

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,60 @@ import react from '@vitejs/plugin-react'
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
 
 const pwaOptions: Partial<VitePWAOptions> = {
+    registerType: 'autoUpdate',
     workbox: {
         globPatterns: ["**/*"],
     },
     includeAssets: [
         "**/*",
     ],
-
+    manifest: {
+        "short_name": "Wordle 🇮🇹",
+        "name": "Wordle 🇮🇹",
+        "description": "Indovina la parola nascosta in 6 tentativi. Gioca con piu' di 3000 parole italiane.",
+        "start_url": "./Wordle-ita/",
+        "scope": "./Wordle-ita/",
+        "theme_color":"#242424",
+        "background_color": "#242424",
+        "display": "standalone",
+        "orientation": "portrait",
+        "icons": [{
+            "src": "/Wordle-ita/icons/wordle_logo_512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        },
+        {
+            "src": "/Wordle-ita/icons/wordle_logo_192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/Wordle-ita/icons/wordle_logo_144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+        },
+        {
+            "src": "/Wordle-ita/icons/wordle_logo_32x32.png",
+            "sizes": "32x32",
+            "type": "image/png"
+        },
+        {
+            "src": "/Wordle-ita/icons/wordle-favicon.ico",
+            "sizes": "32x32",
+            "type": "image/x-icon"
+        },
+        {
+            "src": "/Wordle-ita/icons/wordle-apple-touch-icon.png",
+            "sizes": "180x180",
+            "type": "image/png"
+        },
+        {
+            "src": "/Wordle-ita/icons/maskable-icon.svg",
+            "sizes": "512x512",
+            "type": "image/svg+xml",
+            "purpose": "maskable"
+        }]
+    }
 }
 
 // https://vitejs.dev/config/
