06ee1bce276e7b7b24c65ea97bcfd3f481bedcf1
Alessio Poggi <alessio.poggi01@universitadipavia.it>
2023-09-21 16:17:19 +0200

    added pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
+
+const pwaOptions: Partial<VitePWAOptions> = {
+    workbox: {
+        globPatterns: ["**/*"],
+    },
+    includeAssets: [
+        "**/*",
+    ],
+
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Wordle-ita",
-  plugins: [react()],
+    base: "/Wordle-ita",
+    plugins: [react(), VitePWA(pwaOptions)],
 })
